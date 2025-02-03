f03de310801854293a040af05d144532a5ef05c9
wijaya060906 <wijayayusan9@gmail.com>
2025-02-03 09:30:52 +0700

    Yusan Developer Handal

--- resources/views/admin/undangan/undangan.blade.php
+++ resources/views/admin/undangan/undangan.blade.php
@@ -50,7 +50,7 @@
             <div class="layout-page">
               <div class="content-wrapper">
                 <div class="container-xxl flex-grow-1 container-p-y">
-                        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Tables /</span> Basic Tables
+                        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Tables /</span> Surat Undangan
                         </h4>
                         <div class="card">
                             <h5 class="card-header">Table Basic</h5>
--- resources/views/user/surat tugas/suratTugas.blade.php
+++ resources/views/user/surat tugas/suratTugas.blade.php
@@ -89,7 +89,7 @@
           <div class="content-wrapper">
             <!-- Content -->
             <div class="container-xxl flex-grow-1 container-p-y">
-              <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Tables /</span> Basic Tables</h4>
+              <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Tables /</span> Surat Tugas</h4>
               <!-- Basic Bootstrap Table -->
               <div class="card">
                 <h5 class="card-header">Table Basic</h5>
